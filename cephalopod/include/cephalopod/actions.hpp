435364ddfc58a59a63b534f928736bd43c784041
jwezorek <jwezorek@gmail.com>
2017-11-18 12:16:37 -0800

    fleshing out actions a little. not sure i'll end up keeping the distinction between finite and infite actions.

--- cephalopod/include/cephalopod/actions.hpp
+++ cephalopod/include/cephalopod/actions.hpp
@@ -1,18 +1,48 @@
 #pragma once
 
 #include "signals.hpp"
+#include "actor.hpp"
 
 namespace ceph {
 
 	class Action : public Slot<Action>
 	{
 	private:
-		Signal<const Action*> complete_event_;
-
+		std::weak_ptr<Actor> owner_;
+		bool is_paused_;
+		std::vector<std::shared_ptr<Action>> children_;
 	public:
-		Action();
-		virtual ~Action();
+		Action(bool startPaused = false);
+
+		virtual void pause();
+		virtual void unpause();
+		bool isPaused() const;
+		bool hasChildren() const;
+
+		void attach(const std::shared_ptr<Actor> owner);
+		void unattach();
+		bool isAttached() const;
+
+		virtual void initialize() = 0;
+		virtual void update(float elapsed) = 0;
+
+		~Action();
 	};
 
+	class FiniteAction : public Action
+	{
+	private:
+		Signal<const Action*> complete_event_;
+		float elapsed_;
+		float duration_;
+		bool is_complete_;
+	public:
+		FiniteAction(float duration);
+
+		virtual bool isComplete() const;
+		virtual void initialize() override;
+		virtual void update(float elapsed) override;
+		virtual void updatePcntComplete(float pcnt) = 0;
+	};
 
 }
--- cephalopod/source/actions.cpp
+++ cephalopod/source/actions.cpp
@@ -1,8 +1,88 @@
 #include "../include/cephalopod/actions.hpp"
 
-ceph::Action::Action() {}
+ceph::Action::Action(bool startPaused)
+{
+	is_paused_ = startPaused;
+	owner_ = std::weak_ptr<Actor>();
+}
+
+void ceph::Action::attach(const std::shared_ptr<Actor> owner)
+{
+	owner_ = owner;
+	for (auto& child : children_)
+		child->attach(owner);
+}
+
+void ceph::Action::unattach()
+{
+	owner_ = std::weak_ptr<Actor>();
+	for (auto& child : children_)
+		child->unattach();
+}
+
+bool ceph::Action::isAttached() const
+{
+	return owner_.expired();
+}
+
+void ceph::Action::pause()
+{
+	is_paused_ = true;
+	for (auto& child : children_)
+		child->pause();
+}
+
+void ceph::Action::unpause()
+{
+	is_paused_ = false;
+	for (auto& child : children_)
+		child->unpause();
+}
+
+bool ceph::Action::isPaused() const
+{
+	return is_paused_;
+}
+
+bool ceph::Action::hasChildren() const
+{
+	return !children_.empty();
+}
 
 ceph::Action::~Action()
 {
+}
 
+/*--------------------------------------------------------------------------------*/
+
+ceph::FiniteAction::FiniteAction(float duration)
+{
+	duration_ = duration;
+	initialize();
+}
+
+void ceph::FiniteAction::initialize() 
+{
+	elapsed_ = 0.0f;
+	is_complete_ = false;
+}
+
+void ceph::FiniteAction::update(float elapsed)
+{
+	if (isComplete() || isPaused())
+		return;
+	elapsed_ += elapsed;
+	float pcnt_complete = elapsed_ / duration_;
+	if (pcnt_complete >= 1.0f) 
+		pcnt_complete = 1.0f;
+	updatePcntComplete(pcnt_complete);
+	if (pcnt_complete == 1.0f) {
+		is_complete_ = true;
+		complete_event_.fire(this);
+	}
+}
+
+bool ceph::FiniteAction::isComplete() const
+{
+	return is_complete_;
 }
