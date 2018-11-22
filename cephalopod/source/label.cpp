c6fd2037702e82ba5edb8c94b3023b54d0201829
jwezorek <jwezorek@gmail.com>
2018-11-22 08:53:36 -0800

    removed some dead code

--- cephalopod/source/label.cpp
+++ cephalopod/source/label.cpp
@@ -53,8 +53,7 @@ ceph::Label::Label(const std::shared_ptr<FontSheet> fs, const std::string & font
 	font_sz_(font_sz),
 	text_(text),
 	just_(just)
-{
-}
+{}
 
 ceph::Label::Label(const std::shared_ptr<FontSheet> fs, const std::string& font_key, const std::string& text, Justification just) :
 	font_sheet_(fs),
@@ -62,8 +61,7 @@ ceph::Label::Label(const std::shared_ptr<FontSheet> fs, const std::string& font_
 	font_sz_(0),
 	text_(text),
 	just_(just)
-{
-}
+{}
 
 void ceph::Label::initialize()
 {
@@ -100,40 +98,3 @@ void ceph::Label::initialize()
 	}
 }
 
-/*
-void ceph::Label::initialize()
-{
-	std::string key = (font_sz_ > 0) ? font_key_ + "_" + std::to_string(font_sz_) : font_key_;
-	auto& font_item = font_sheet_->getFont( key );
-	const auto& font = *font_item.font;
-	float scale = font.getScaleForPixelHeight(font_item.size);
-	auto metrics = font.getMetrics();
-
-	float ascent = metrics.ascent * scale;
-	float descent = metrics.descent * scale;
-	float x = 0, line_y = 0;
-
-	for (int i = 0; i < text_.size(); i++) {
-		char ch = text_[i];
-		char next_ch = text_[i + 1];
-
-		float character_y = line_y - font.getCharacterBoundingBox(ch, scale, scale).y2();
-
-		if (std::isgraph(ch)) {
-			auto sprite = font_sheet_->getSprite(font_key_, font_sz_, ch);
-			sprite->setAnchorPcnt(0, 0);
-			float character_x = x + scale * font.getCharacterLeftSideBearing(ch);
-			sprite->moveTo(character_x, character_y);
-			addChild(sprite);
-		}
-
-		if (ch != '\n') {
-			x += scale * (font.getCharacterAdvance(ch) + font.getKernAdvance(ch, next_ch));
-		} else {
-			x = 0;
-			// should be equal to font_sz_ + line_gap I guess...
-			line_y -= (metrics.ascent - metrics.descent + metrics.line_gap) * scale; 
-		}
-	}
-}
-*/
